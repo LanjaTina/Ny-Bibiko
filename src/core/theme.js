f0466251b41950561afef956e26924e269ab9a84
LanjaTina Randriananahary <111855158+LanjaTina@users.noreply.github.com>
2024-05-11 12:45:26 +0300

    Initial commit

---
+++ src/core/theme.js
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'react-native-paper'
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: '#000000',
+    secondary: '#D0993c',
+    primary: '#4A4653',
+    error: '#f13a59',
+  },
+}
