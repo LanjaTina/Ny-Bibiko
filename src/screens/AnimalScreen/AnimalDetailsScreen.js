e334b3f9f99d0ef09ebe67d5f5d1ef32e3725cc3
tolotraRan <ranarisontolotra@gmail.com>
2024-06-14 19:50:58 +0200

    un peu d'amour pour la nourriture

--- src/screens/AnimalScreen/AnimalDetailsScreen.js
+++ src/screens/AnimalScreen/AnimalDetailsScreen.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { View, Text, StyleSheet, Image, ScrollView, Dimensions, TouchableOpacity } from 'react-native';
-import { useDispatch } from 'react-redux';
-import { removeAnimal } from '../../store/reducers/animalSlice';
-import {theme} from "../../core/theme"
+import { useDispatch, useSelector } from 'react-redux';
+import { removeAnimal, addToFavorites, removeFromFavorites, selectFavorites } from '../../store/reducers/animalSlice';
+import { FontAwesome5 } from '@expo/vector-icons';
+import LottieView from 'lottie-react-native';
+import { theme } from '../../core/theme';
 
 const { width } = Dimensions.get('window');
 
@@ -10,6 +12,9 @@ const AnimalDetailsScreen = ({ route, navigation }) => {
   const { animal } = route.params;
   const dispatch = useDispatch();
   const [activeIndex, setActiveIndex] = useState(0);
+  const favorites = useSelector(selectFavorites);
+  const [showHeartAnimation, setShowHeartAnimation] = useState(false);
+  const scrollViewRef = useRef(null);
 
   const handleDelete = () => {
     dispatch(removeAnimal(animal.id));
@@ -30,8 +35,17 @@ const AnimalDetailsScreen = ({ route, navigation }) => {
     scrollViewRef.current.scrollTo({ x: index * width, animated: true });
   };
 
+  const handleAddToFavorites = (animal) => {
+    if (favorites.some(favAnimal => favAnimal.id === animal.id)) {
+      dispatch(removeFromFavorites(animal.id));
+    } else {
+      dispatch(addToFavorites(animal));
+      setShowHeartAnimation(true); // Activer l'animation de cœur
+      setTimeout(() => setShowHeartAnimation(false), 4000); // Désactiver après 2 secondes
+    }
+  };
+
   const images = [animal.image1, animal.image2, animal.image3, animal.image4];
-  const scrollViewRef = React.useRef(null);
 
   return (
     <View style={styles.container}>
@@ -62,19 +76,30 @@ const AnimalDetailsScreen = ({ route, navigation }) => {
         ))}
       </View>
       <Text style={styles.textName}>{animal.name}</Text>
-      <View style={{
-        backgroundColor:theme.colors.secondary,
-        borderTopLeftRadius: 50,
-        borderTopRightRadius: 50,
-
-      }}>
-        <Text style={styles.text}>Type: {animal.type}</Text>
-        <Text style={styles.text}>Couleur: {animal.color}</Text>
-        <Text style={styles.text}>{animal.description}</Text>
+      <View style={styles.descriptionContainer}>
+        <Text style={styles.textDescription}>{animal.description}</Text>
+        <Text style={styles.text}><Text style={{ fontWeight: "bold", textDecorationLine: "underline" }}>Type:</Text> {animal.type}</Text>
+        <Text style={styles.text}><Text style={{ fontWeight: "bold", textDecorationLine: "underline" }}>Couleur:</Text> {animal.color}</Text>
+        
+          <FontAwesome5
+            name={favorites.some(favAnimal => favAnimal.id === animal.id) ? 'heart' : 'heart'}
+            solid={favorites.some(favAnimal => favAnimal.id === animal.id)}
+            style={[
+              styles.heartIcon,
+              favorites.some(favAnimal => favAnimal.id === animal.id) ? { color: '#cd3b25' } : { color: '#cd3b25' }
+            ]}
+            size={25}
+            onPress={() => handleAddToFavorites(animal)}
+          />
+        {showHeartAnimation && (
+          <LottieView
+            source={require('../../../assets/heart.json')}
+            autoPlay
+            loop={false}
+            style={styles.heartAnimation}
+          />
+        )}
       </View>
-      
-      {/* <Button title="Modifier" onPress={handleEdit} />
-      <Button title="Supprimer" onPress={handleDelete} /> */}
     </View>
   );
 };
@@ -85,14 +110,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   scrollView: {
-    height: 300, 
+    height: 250,
     width: '100%',
     borderBottomLeftRadius: 50,
     borderBottomRightRadius: 50,
+    marginBottom: 10,
   },
   image: {
     width: width,
-    height: 250, 
+    height: '100%',
     resizeMode: 'cover',
     borderBottomLeftRadius: 50,
     borderBottomRightRadius: 50,
@@ -100,7 +126,6 @@ const styles = StyleSheet.create({
   indicatorContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
-    marginTop: 10,
   },
   thumbnail: {
     width: 50,
@@ -118,14 +143,52 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 18,
-    color:"white",
+    color: 'white',
     marginVertical: 10,
+    textAlign: 'justify',
+    paddingHorizontal: 20,
+  },
+  textDescription: {
+    fontSize: 18,
+    color: 'white',
+    marginVertical: 10,
+    textAlign: 'justify',
+    paddingHorizontal: 20,
   },
   textName: {
-    color:"white",
-    fontWeight:"bold",
+    color: 'white',
+    fontWeight: 'bold',
     fontSize: 30,
     marginVertical: 10,
+  },
+  descriptionContainer: {
+    backgroundColor: theme.colors.secondary,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    marginBottom: '20%',
+    width: '100%',
+    paddingBottom: 10,
+    paddingHorizontal: 20,
+    position: 'relative',
+  },
+  heartIcon: {
+    position: 'absolute',
+    bottom: 10,
+    right: 20,
+  },
+  favoriteButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+  },
+  heartAnimation: {
+    position: 'absolute',
+    bottom: -50,
+    right: 0,
+    width: "100%",
+    height: "100%",
   },
 });
 
